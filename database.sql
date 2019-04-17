4379718ccfbe7092eccf15fb0da6471036abc1f9
Javi Chulvi <al364498@uji.es>
2019-04-17 17:37:24 +0200

    database.sql
    añado el .sql aquí para que nos sea más fácil tener la última versión actualizada

---
+++ database.sql
@@ -0,0 +1,127 @@
+CREATE TABLE TipoActividad (
+    id VARCHAR(60) NOT NULL,
+	nombre VARCHAR(50) NOT NULL,
+	nivel VARCHAR(15) NOT NULL,
+	CONSTRAINT cp_tipoactividad PRIMARY KEY (id),
+	CONSTRAINT ri_tipoactividad_nivel CHECK (nivel='bajo' OR nivel='medio' OR nivel='alto' OR nivel='extremo')
+);
+
+CREATE TABLE Actividad (
+    id SERIAL NOT NULL,
+    idTipoActividad VARCHAR(20) NOT NULL,
+    estado VARCHAR(15) NOT NULL,
+    nombre VARCHAR(40) NOT NULL,
+    descripcion VARCHAR(100) NOT NULL,
+    duracion TIME NOT NULL,
+    fecha DATE NOT NULL,
+    minAsistentes INTEGER NOT NULL,
+    maxAsistentes INTEGER NOT NULL,
+    lugar VARCHAR(40) NOT NULL,
+    puntoDeEncuentro VARCHAR(30) NOT NULL,
+    horaDeEncuentro TIME NOT NULL,
+    CONSTRAINT cp_actividad PRIMARY KEY (id),
+    CONSTRAINT ca_actividad FOREIGN KEY (idTipoActividad) REFERENCES TipoActividad(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT ri_actividad_duracion CHECK (duracion>'00:00'),
+    CONSTRAINT ri_actividad_estado CHECK (estado='abierta' OR estado='cerrada' OR estado='completa' OR estado='cancelada')
+);
+    
+CREATE TABLE Monitor (
+    dni VARCHAR(10) NOT NULL,
+    estado VARCHAR(15) NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
+    domicilio VARCHAR(70) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    iban VARCHAR(20) NOT NULL,
+    foto VARCHAR(50) NOT NULL,
+    contraseña VARCHAR(20) NOT NULL,
+    CONSTRAINT cp_monitor PRIMARY KEY (dni),
+    CONSTRAINT ri_monitor_estado CHECK (estado='aceptada' OR estado='rechazada' OR estado='pendiente')
+);
+    
+CREATE TABLE Acreditacion (
+	certificado VARCHAR(50) NOT NULL,
+	dniMonitor VARCHAR(10) NOT NULL,
+	estado VARCHAR(15),
+	CONSTRAINT cp_certificado PRIMARY KEY (certificado),
+	CONSTRAINT ca_monitor FOREIGN KEY (dniMonitor) REFERENCES Monitor(dni) ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT ri_acreditacion_estado CHECK (estado='aceptada' OR estado='rechazada')
+);
+
+CREATE TABLE Acredita(
+    idTipoActividad VARCHAR(20) NOT NULL,
+    certificado VARCHAR(50) NOT NULL,
+    CONSTRAINT cp_acredita PRIMARY KEY (idTipoActividad, certificado),
+    CONSTRAINT ca_tipoactividad FOREIGN KEY (idTipoActividad) REFERENCES TipoActividad(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT ca_acreditacion FOREIGN KEY (certificado) REFERENCES Acreditacion(certificado) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE Cliente (
+    dni VARCHAR(10) NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    sexo VARCHAR(10)NOT NULL,
+    fechaNacimiento DATE NOT NULL,
+    contraseña VARCHAR(20) NOT NULL,
+    CONSTRAINT cp_client PRIMARY KEY (dni)
+);
+    
+CREATE TABLE Oferta (
+	idActividad INTEGER NOT NULL,
+	dniMonitor VARCHAR(10) NOT NULL,
+	CONSTRAINT cp_oferta PRIMARY KEY (idActividad, dniMonitor),
+	CONSTRAINT ca_actividad FOREIGN KEY (idActividad) REFERENCES Actividad(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT ca_monitor FOREIGN KEY (dniMonitor) REFERENCES Monitor(dni) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+
+
+
+CREATE TABLE Prefiere (
+	dniCliente VARCHAR(10) NOT NULL,
+	idTipoActividad VARCHAR (20) NOT NULL,
+	CONSTRAINT cp_prefiere PRIMARY KEY (dniCliente, idTipoActividad),
+	CONSTRAINT ca_tipoactividad FOREIGN KEY (idTipoActividad) REFERENCES TipoActividad(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT ca_cliente FOREIGN KEY (dniCliente) REFERENCES Cliente(dni) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE ImagenPromocional (
+	idActividad INTEGER NOT NULL,
+	imagen VARCHAR(50) NOT NULL,
+	CONSTRAINT cp_imagenpromocional PRIMARY KEY (idActividad, imagen),
+	CONSTRAINT ca_actividad FOREIGN KEY (idActividad) REFERENCES Actividad(id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+
+
+CREATE TABLE Reserva (
+	numTransaccion INTEGER NOT NULL,
+	idActividad INTEGER NOT NULL,
+	dniCliente VARCHAR(10) NOT NULL,
+	estadoPago VARCHAR(15) NOT NULL,
+	fecha DATE NOT NULL,
+	numAsistentes INTEGER NOT NULL,
+	precioPorPersona FLOAT NOT NULL,
+	precioTotal FLOAT NOT NULL,
+	CONSTRAINT cp_reserva PRIMARY KEY (numTransaccion),
+	CONSTRAINT ca_actividad FOREIGN KEY (idActividad) REFERENCES Actividad(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT ca_cliente FOREIGN KEY (dniCliente) REFERENCES Cliente(dni) ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT ri_reserva_estadoPago CHECK (estadoPago='pendiente' OR estadoPago='pagado')
+);
+
+CREATE TABLE Entrada (
+    idActividad INTEGER NOT NULL,
+    tipo VARCHAR(15) NOT NULL,
+    precio FLOAT NOT NULL,
+    CONSTRAINT cp_entrada PRIMARY KEY (idActividad, tipo),
+    CONSTRAINT ca_actividad FOREIGN KEY (idActividad) REFERENCES Actividad(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT ri_entrada_tipo CHECK (tipo='menor18' OR tipo='entre18-50' OR tipo='mayor50' OR tipo='grupo' OR tipo='temporadabaja' OR tipo='temporadaalta')
+);
+
+CREATE TABLE TextoCliente (
+    id SERIAL NOT NULL,
+	idActividad INTEGER NOT NULL,
+	fecha DATE NOT NULL,
+	mensaje VARCHAR(150) NOT NULL,
+	CONSTRAINT cp_textocliente PRIMARY KEY (id),
+	CONSTRAINT ca_actividad FOREIGN KEY (idActividad) REFERENCES Actividad(id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
